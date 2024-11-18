e716bc878cccfc607bf9bb5bed00b4ca3dac65a1
AlinaRamazanova <ramazanovaalina327@yandex.ru>
2024-11-18 18:32:30 +0500

    Техническое задание проекта №6

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -7,6 +7,6 @@ public class Epic extends Task {
    public List<Integer> subtaskIds = new ArrayList<>();
 
    public Epic (String title, String description) {
-      super(title,description);
+      super(title, description);
    }
 }
